827c4bc2824be2b4b482c220670eef1f69a1ee24
YE <yassine.elkabaili@gmail.com>
2022-08-29 22:25:01 -0400

    added big java book source codes

---
+++ chapter02/src/chapter02/PointTester.java
@@ -0,0 +1,15 @@
+package chapter02;
+import java.awt.Point;
+
+public class PointTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point point1 = new Point(3, 4);
+		Point point2 = new Point(-3, -4);
+		
+		System.out.println(point1.distance(point2));
+		
+	}
+
+}
