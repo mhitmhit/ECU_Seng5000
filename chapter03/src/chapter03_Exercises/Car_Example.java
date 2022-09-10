efee9048dbda36565d1ac105f2a9ed1e681be6a9
Y.E <yassine.elkabaili@gmail.com>
2022-09-10 10:39:30 -0400

    chapter 3 examples

---
+++ chapter03/src/chapter03_Exercises/Car_Example.java
@@ -0,0 +1,22 @@
+package chapter03_Exercises;
+
+import javax.swing.JFrame;
+
+
+public class Car_Example {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// program that draws two squares with same center
+		// Initialize Frame
+	      JFrame window = new JFrame();
+
+	      // Start Frame
+	      window.setSize(400, 300);
+	      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	      // Draw Component
+	      window.add(new Component_Example());
+
+	      window.setVisible(true);
+	}
+}
