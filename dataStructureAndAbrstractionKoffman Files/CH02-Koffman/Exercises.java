266891c6b82bb783352dcd6e81f902af9cfea9d9
Y.E <yassine.elkabaili@gmail.com>
2022-09-25 18:51:49 -0400

    created chapter 02 folders including source files from the book

---
+++ dataStructureAndAbrstractionKoffman Files/CH02-Koffman/Exercises.java
@@ -0,0 +1,13 @@
+package KW.CH02;
+
+import java.util.ArrayList;
+
+/**
+ * Class to contain static method exercise solutions
+ */
+public class Exercises {
+
+// Insert solution to programming exercise 1, section 2, chapter 2 here
+
+// Insert solution to programming exercise 2, section 2, chapter 2 here
+}
