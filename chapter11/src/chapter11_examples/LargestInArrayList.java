a3414eed52acd913f3fb661b860bdc29e99f2036
Y.E <yassine.elkabaili@gmail.com>
2022-10-02 10:46:16 -0400

    fixed chapter 7 errors

---
+++ chapter11/src/chapter11_examples/LargestInArrayList.java
@@ -0,0 +1,19 @@
+package chapter11_examples;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+   This program reads a sequence of values and prints them, marking the largest value.
+*/
+public class LargestInArrayList
+{
+   public static void main(String[] args)
+   {  
+      String input ="test 55 66";
+      Scanner in = new Scanner(input);
+      System.out.println(in.nextInt());
+      //System.out.println(in.next());
+      int n = in.nextInt();
+      
+   }
+}
