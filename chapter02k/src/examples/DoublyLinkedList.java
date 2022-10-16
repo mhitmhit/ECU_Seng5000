a80fc27aedf729e11e5731be5fd96ccb9b59fd50
Y.E <yassine.elkabaili@gmail.com>
2022-10-16 12:24:23 -0400

    chapter doubly linked list example

--- chapter02k/src/examples/DoublyLinkedList.java
+++ chapter02k/src/examples/DoublyLinkedList.java
@@ -2,17 +2,64 @@ package examples;
 
 public class DoublyLinkedList<E> {
 
+	private ListNode head;
+	private ListNode tail;
+	private int length;
+	
 	private class ListNode {
 		private E data;
-		private listNode<E> next = null;
-		private listNode<E> prev = null;
+		private ListNode next = null;
+		private ListNode prev = null;
 
+		private ListNode(E data) {
+			this.data = data;
+			
+		}
+	}
+	
+	public DoublyLinkedList() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+	
+	public boolean isEmpty() {
+		return length == 0; // head == null;
+	}
+	
+	public int length() {
+		return length;
+	}
+	
+	public void display() {
+		ListNode temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + " --> ");
+			temp = temp.next;
+		}
+		System.out.print("null");
+	}
+	
+	public void addLast(E value) {
+		ListNode newNode = new ListNode(value);
+		if (isEmpty()) {
+			head = newNode;
+		}else {
+			tail.next = newNode;
+		}
+		newNode.prev = tail;
+		tail = newNode;
+		length++;
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		DoublyLinkedList<Integer> doubly = new DoublyLinkedList<Integer>();
+		doubly.addLast(10); // head
+		doubly.addLast(12);
+		doubly.addLast(13);
+		doubly.display();
 		
 	}
-
+	
 }
