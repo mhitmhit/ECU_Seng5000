9fee2f17634371252f80e430c98f0241a62f3f2a
YE <yassine.elkabaili@gmail.com>
2022-10-27 19:35:48 -0400

    Assignment 4 questino 2 started

--- Assignment_4/src/A4_Q2/A4_Q2.java
+++ Assignment_4/src/A4_Q2/A4_Q2.java
@@ -13,22 +13,27 @@ public class A4_Q2 {
 		 * showing the operand stack each time it is
 		 * modified
 		 */
-		System.out.println("       Expression "+"          Action  "+"       Stack");
+		System.out.println("       Expression "+"          Action  "+"           Stack");
 		System.out.println("| ");
-		System.out.println("13 2 * 5 / 6 2 5 * - + "+"      push 13"+"        [13]"); 
+		System.out.println("13 2 * 5 / 6 2 5 * - + "+"      push 13"+"            [13]"); 
 		System.out.println("   | ");
-		System.out.println("13 2 * 5 / 6 2 5 * - + "+"      push 2"+"        [2, 13]");
+		System.out.println("13 2 * 5 / 6 2 5 * - + "+"      push 2"+"             [2, 13]");
 		System.out.println("     | ");
-		System.out.println("13 2 * 5 / 6 2 5 * - + "+"    pop 2 and 13");
-		System.out.println("                       "+"    mult 2 and 13");
-		System.out.println("                       "+"    push 26"+"           [26]");
+		System.out.println("13 2 * 5 / 6 2 5 * - + "+"      pop 2 and 13");
+		System.out.println("                       "+"      mult 2 and 13");
+		System.out.println("                       "+"      push 26"+"            [26]");
 		System.out.println("       | ");
-		System.out.println("13 2 * 5 / 6 2 5 * - + "+"      push 5"+"        [5, 26]");
+		System.out.println("13 2 * 5 / 6 2 5 * - + "+"      push 5"+"             [5, 26]");
 		System.out.println("         | ");
-		System.out.println("13 2 * 5 / 6 2 5 * - + "+"    pop 5 and 26");
-		System.out.println("                       "+"   divde 26 by 5");
-		System.out.println("                       "+"    push 5"+"            [5]");
-		
+		System.out.println("13 2 * 5 / 6 2 5 * - + "+"      pop 5 and 26");
+		System.out.println("                       "+"      divde 26 by 5");
+		System.out.println("                       "+"      push 5"+"             [5]");
+		System.out.println("           | ");
+		System.out.println("13 2 * 5 / 6 2 5 * - + "+"      push 6"+"             [6, 5]");
+		System.out.println("             | ");
+		System.out.println("13 2 * 5 / 6 2 5 * - + "+"      push 2"+"             [2, 6, 5]");
+		System.out.println("               | ");
+		System.out.println("13 2 * 5 / 6 2 5 * - + "+"      push 5"+"             [5, 2, 6, 5]");
 		
 	}
 
