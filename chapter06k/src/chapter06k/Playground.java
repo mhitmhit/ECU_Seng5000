7fad4eeac4d03ca94c2f11070cefb83df833974a
YE <yassine.elkabaili@gmail.com>
2022-11-12 12:44:44 -0500

    ready to start assignment number 5

---
+++ chapter06k/src/chapter06k/Playground.java
@@ -0,0 +1,13 @@
+package chapter06k;
+
+public class Playground {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinaryTree<String> Tree = new BinaryTree<>("a");
+		System.out.println(Tree.getRootData());
+		
+		
+	}
+
+}
