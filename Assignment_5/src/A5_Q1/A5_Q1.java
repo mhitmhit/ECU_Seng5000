8aada6df37016c04785316a70c78a7ed5e504186
YE <yassine.elkabaili@gmail.com>
2022-11-13 15:55:54 -0500

    assignment 5 classes created

---
+++ Assignment_5/src/A5_Q1/A5_Q1.java
@@ -0,0 +1,10 @@
+package A5_Q1;
+
+public class A5_Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Assignment_5/src/A5_Q2/A5_Q2.java
@@ -0,0 +1,10 @@
+package A5_Q2;
+
+public class A5_Q2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
